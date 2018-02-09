f99716df8b21f41a17f485561cb52e9603728e5c
emmadema <emma.demango@gmail.com>
2018-02-08 23:34:38 -0700

    adding apis

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@
 //Make: Create a new component. This component should produce some HTMl
 
 //Show on page: Tkae this components gnerated HTMl and put it in the DOM
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC9bLal_hxKk-zOwrdtGCb_zPh9ld5v0y0';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			//this.setState({videos: videos})
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
